aa031b6e1c238c53cb8b613aef12d769ef5c90b8
Lukas Schmidt <lukas.schmi@gmail.com>
2016-01-11 12:35:30 +0100

    clean up

---
+++ Implementierung/Medications/DemoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DemoViewController.swift
+//  Medications
+//
+//  Created by Lukas Schmidt on 10.01.16.
+//  Copyright © 2016 Lukas Schmidt. All rights reserved.
+//
+
+import UIKit
+
+class DemoViewController: UIViewController {
+
+}
